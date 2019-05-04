bd8b50954c28af549c4321bde82186550567b0c3
Tyler <williamstyler707@gmail.com>
2019-05-03 21:15:09 -0400

    javascriptAdded

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MealSpot
-project-1
+This is a website for people who want to learn how to cook healthy foods and for those who want to eat healthy but don't have time to cook.
+Users will be able to choose between two categories: Recipes and Restaurants. 
+Depending on what they pick, they will have a search bar where they can type any kind of food or cuisine they want (using keywords).
+Once the results appear, the users can pick one of the suggested recipes or restaurants respectively.
+After making their selection, they will have the choice to add it to their calendar if they please so they can re visit it whenever they want.
+Simple, easy to use, and effective.
+
---
+++ assets/javascript/script.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    function runQuery(queryURL) {
+        $.ajax({
+            url: queryURL,
+            type: "GET",
+            headers: { 'user-key': '542de442a516e41fb48a13a6f2917dfb' },
+            contentType: "application/JSON",
+        }).then(function (response) {
+            $("#restaurantDisplay").empty();
+            console.log(response);
+
+
+            for (var i = 0; i < 16; i++) {
+                console.log(response);
+                console.log(response.restaurants[i].restaurant.name);
+                console.log(response.restaurants[i].restaurant.currency);
+                console.log(response.restaurants[i].restaurant.location.address);
+                console.log(response.restaurants[i].restaurant.menu_url);
+                console.log(response.restaurants[i].restaurant.photos_url);
+
+                var restaurantDisplay = $("<div>");
+                restaurantDisplay.addClass("well");
+                restaurantDisplay.attr("id", "restaurantWell-" + i);
+                $("#restaurantDisplay").append(restaurantDisplay);
+
+                $("#restaurantWell-" + i).append("<h4> Establishment: " + response.restaurants[i].restaurant.name + "</h4>");
+                $("#restaurantWell-" + i).append("<h6> Price Range (max $$$$): " + response.restaurants[i].restaurant.currency + "</h4>");
+                $("#restaurantWell-" + i).append("<h5> Cuisine: " + response.restaurants[i].restaurant.cuisines + "</h5>");
+                $("#restaurantWell-" + i).append("<h5> Address: " + response.restaurants[i].restaurant.location.address + "</h5>");
+                $("#restaurantWell-" + i).append('<a href="'  + response.restaurants[i].restaurant.menu_url + '" target="_blank">Click to view menu </a>');
+            }
+        }) //closes .then(function (response)
+    };
+
+
+
+    function searchRecipe(queryURLrecipe) {
+      $.ajax({
+        url: queryURLrecipe ,
+        method: "GET"
+      }).then(function(response) {
+        $("#recipeDisplay").empty();
+
+            for (var i = 0; i < 16; i++) { 
+                console.log(response);
+          console.log(response.hits[i].recipe.label);
+          console.log(response.hits[i].recipe.healthLabels);
+          console.log(response.hits[i].recipe.calories);
+          console.log(response.hits[i].recipe.ingredients);
+          console.log(response.hits[i].recipe.image);
+
+          var recipeDisplay = $("<div>");
+          recipeDisplay.addClass("well");
+          recipeDisplay.attr("id", "recipeWell-" + i);
+          $("#recipeDisplay").append(recipeDisplay);
+
+          $("#recipeWell-" + i).append("<h4>Recipe Name: " + response.hits[i].recipe.label +  "</h4>");
+          $("#recipeWell-" + i).append('<a href="'  + response.hits[i].recipe.url + '" target="_blank">Click to view full recipe </a>');
+          $("#recipeWell-" + i).append("<h6>Health Labels: " + response.hits[i].recipe.healthLabels +  "</h6>");
+          $("#recipeWell-" + i).append('<img src="' + response.hits[i].recipe.image + '">');
+
+          for (var j = 0; j < response.hits[i].recipe.ingredients.length; j++) {
+          $("#recipeWell-" + i).append("<h6>" + response.hits[i].recipe.ingredients[j].text +  "</h6>");
+
+          }
+        }
+    }) //closes .then(function (response)
+};
+          
+        
+
+    $("#submit").on("click", function () {
+        var queryTerm = $("#searchArea").val().trim();
+        console.log(queryTerm);
+
+        var recipe = $('#recipe').is(":checked")
+
+        var restaurant = $('#restaurant').is(":checked")
+
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&count=15";
+
+        var queryURLrecipe = "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?app_id=da06b7eb&app_key=3b93ed52b89ba41f57fb99a692028518&to=15&q=" + queryTerm;
+
+        var newURL = queryURL + "&q=" + queryTerm;
+        console.log(newURL);
+        
+
+       
+
+//If only Restaurant box selected
+        if (!recipe && restaurant) {
+            runQuery(newURL);
+
+
+//Only recipe box selected
+        } else if (recipe && !restaurant) {
+            searchRecipe(queryURLrecipe);
+     
+
+//Both restaurant & recipe box selected
+        } else if (restaurant && recipe) {
+            runQuery(newURL);
+            searchRecipe(queryURLrecipe);
+
+        };
+    }) //closes submit on click function
+}); //closes doc ready function
+Collapse
+
+
+
